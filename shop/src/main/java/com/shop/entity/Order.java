52c0f922c72d371a6c3cd7c827c9a98b9fdeccd6
Junho Kim <chordplaylist@gmail.com>
2022-10-02 02:18:26 +0900

    22_10_02 shop with Boot p.212

---
+++ shop/src/main/java/com/shop/entity/Order.java
@@ -0,0 +1,36 @@
+package com.shop.entity;
+
+import com.shop.constant.OrderStatus;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "orders")
+@Getter @Setter
+public class Order {
+
+    @Id @GeneratedValue
+    @Column(name = "order_id")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    private LocalDateTime localDateTime;
+
+    @Enumerated(EnumType.STRING)
+    private OrderStatus status;
+
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<OrderItem> orderItems = new ArrayList<>();
+
+    private LocalDateTime regtime;
+
+    private LocalDateTime updateTime;
+}
