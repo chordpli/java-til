fa0463a5132bf71b8d32b2c92c8fbe005b4f75b8
Junho Kim <chordplaylist@gmail.com>
2022-10-01 21:59:33 +0900

    22_10_01 Hash

--- techit_study/src/HashTable.java
+++ techit_study/src/HashTable.java
@@ -1,16 +1,19 @@
 import java.util.LinkedList;
 
 public class HashTable {
-    class Node{
+    class Node {
         String key;
         String value;
+
         public Node(String key, String value) {
             this.key = key;
             this.value = value;
         }
+
         String value() {
             return value;
         }
+
         void value(String value) {
             this.value = value;
         }
@@ -26,21 +29,21 @@ public class HashTable {
 
     // Key값을 통해 HashCode 생성 - 아스키코드이용
     int gethashCode(String key) {
-        int hashcode=0;
-        for(char c : key.toCharArray())
-            hashcode+=c;
+        int hashcode = 0;
+        for (char c : key.toCharArray())
+            hashcode += c;
         return hashcode;
     }
 
     // HashCode를 이용해서 Index를 지정
     int convertToIndex(int hashcode) {
-        return hashcode%data.length;
+        return hashcode % data.length;
     }
 
     Node searchKey(LinkedList<Node> list, String key) {
-        if(list == null ) return null;
-        for(Node node : list) {
-            if(node.key.equals(key)) {
+        if (list == null) return null;
+        for (Node node : list) {
+            if (node.key.equals(key)) {
                 return node;
             }
         }
@@ -48,34 +51,34 @@ public class HashTable {
     }
 
     // Key를 통한 값 저장
-    void set(String key,String value) {
+    void set(String key, String value) {
         int index = convertToIndex(gethashCode(key));
         LinkedList<Node> list = data[index];
         // 없으면 저장
-        if(list == null) {
+        if (list == null) {
             list = new LinkedList<Node>();
             data[index] = list;
         }
 
         Node node = searchKey(list, key);
 
-        if(node==null)
-            list.addLast(new Node(key,value));
+        if (node == null)
+            list.addLast(new Node(key, value));
         else
             node.value(value);
 
-        System.out.println("hashcode : "+gethashCode(key)+", index : " +index + ", ");
+        System.out.println("hashcode : " + gethashCode(key) + ", index : " + index + ", ");
     }
 
     // key를 통한 값 호출
     String get(String key) {
         int index = convertToIndex(gethashCode(key));
         LinkedList<Node> list = data[index];
-        if(list == null)
+        if (list == null)
             return "Not Found";
         else {
-            for(Node n : list) {
-                if(n.key.equals(key))
+            for (Node n : list) {
+                if (n.key.equals(key))
                     return n.value;
             }
             return null;
