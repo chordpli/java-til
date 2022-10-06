5a6ad024921744c81a9e307fccc98a48c18a0379
Junho Kim <chordplaylist@gmail.com>
2022-10-06 23:37:56 +0900

    22_10_06

--- scribble/src/test.java
+++ scribble/src/test.java
@@ -1,5 +1,8 @@
-public class test {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
+public class test {
+    public static void main(String[] args){
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     }
 }
