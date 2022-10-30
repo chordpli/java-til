b9dc77f13d988913701c7c675d60ebec5493fece
chordpli <chordplaylist@gmail.com>
2022-10-30 23:22:59 +0900

    221030 우테코프리코스 경험

--- woowacourse/src/main/java/onboarding/Problem1.java
+++ woowacourse/src/main/java/onboarding/Problem1.java
@@ -1,32 +1,32 @@
 package onboarding;
 
+import java.util.Arrays;
 import java.util.List;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
+        Problem1 problem1 = new Problem1();
+        int answer = 0;
 
-        // 왼쪽 페이지가 짝수일때
-        if (pobi.get(0) % 2 == 0 || crong.get(0) % 2 == 0) {
-            return -1;
-        }
-
-        // 페이지가 연달아 있지 않을때
-        if (pobi.get(0) + 1 != pobi.get(1) || crong.get(0) + 1 != crong.get(1)) {
-            return -1;
-        }
-
-        // 두 수 비교 결과
-        Problem1 p = new Problem1();
         try {
-            answer = p.compareNumber(pobi, crong);
-        } catch (Exception e) {
-            answer = -1;
-        }finally {
+            // 왼쪽 페이지가 짝수거나, 페이지가 연결되지 않으면 -1 return;
+            if (pobi.get(0) % 2 == 0 || crong.get(0) % 2 == 0) {
+                answer = -1;
+                return answer;
+            }
+            if (pobi.get(0) + 1 != pobi.get(1) || crong.get(0) + 1 != crong.get(1)) {
+                answer = -1;
+                return answer;
+            }
+            answer = problem1.compareNumber(pobi, crong);
+        } catch (NullPointerException e) {
+            System.out.println(Arrays.toString(e.getStackTrace()));
+        } finally {
             return answer;
         }
     }
 
+    // pobi의 값과 crong의 값을 비교하여 결과를 리턴
     public int compareNumber(List<Integer> pobi, List<Integer> crong) {
         int pobiNumber = returnBigNum(pobi);
         int crongNumber = returnBigNum(crong);
@@ -40,50 +40,39 @@ class Problem1 {
         }
     }
 
-    // 큰 수 리턴
+    // 덧셈과 곱셈 비교 후 큰 수 리턴
     public int returnBigNum(List<Integer> people) {
         // 각 자리 숫자를 모두 더하거나, 모두 곱해 가장 큰 수를 구한다.
-        int plusNum = returnPlusNum(people.get(0), people.get(1));
-        int multipleNum = returnMulipleNum(people.get(0), people.get(1));
+        int leftPage;
+        int rightPage;
 
-        if (plusNum > multipleNum) {
-            return plusNum;
-        }else{
-            return multipleNum;
-        }
+        leftPage = Math.max(returnMulipleNum(people.get(0)), returnPlusNum(people.get(0)));
+        rightPage = Math.max(returnMulipleNum(people.get(1)), returnPlusNum(people.get(1)));
 
+        return Math.max(leftPage, rightPage);
     }
 
-    // 더하기 큰 수 반환
+    // 각 자릿수를 더한 후 큰 수 반환
+    public int returnPlusNum(int page) {
+        int result = 0;
 
-    public int returnPlusNum(int leftPage, int rightPage) {
-        int sum = 0;
-
-        while (leftPage > 0) {
-            sum += leftPage % 10;
-            leftPage /= 10;
+        while (page > 0) {
+            result += page % 10;
+            page /= 10;
         }
 
-        while (rightPage > 0) {
-            sum += rightPage%10;
-            rightPage /= 10;
-        }
-        return sum;
+        return result;
     }
 
-    // 곱하기 큰 수 반환
-    public int returnMulipleNum(int leftPage, int rightPage) {
-        int sum = 1;
+    // 각 자릿수를 곱하기 큰 수 반환
+    public int returnMulipleNum(int page) {
+        int result = 1;
 
-        while (leftPage > 0) {
-            sum *= leftPage%10;
-            leftPage /= 10;
+        while (page > 0) {
+            result *= page % 10;
+            page /= 10;
         }
 
-        while (rightPage > 0) {
-            sum *= rightPage%10;
-            rightPage /= 10;
-        }
-        return sum;
+        return result;
     }
 }
