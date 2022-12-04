fae6423cfbdb8e327f26461ae2d7980d310b3ff1
chordpli <chordplaylist@gmail.com>
2022-12-04 16:47:43 +0900

    chore: move package

--- algorithm/CodingTest/Baekjoon/bj3003/Main.java
+++ algorithm/CodingTest/Baekjoon/bj3003/Main.java
@@ -1,7 +1,9 @@
+package bj3003;
+
 import java.io.IOException;
 import java.util.Scanner;
 
-public class BJ3003 {
+public class Main {
     public static void main(String[] args) throws IOException {
         Scanner sc = new Scanner(System.in);
 
